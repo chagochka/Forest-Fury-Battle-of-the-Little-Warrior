b80b52b40a553a2a8512d951e94a7fdf38eeb547
chagochka <vad.ermin@yandex.ru>
2024-01-09 21:19:53 +0700

    дописан класс UI

--- UI.py
+++ UI.py
@@ -1,11 +1,10 @@
 import pygame
 
 class UI:
-	def __init__(self, window, font, player, monsters):
+	def __init__(self, window, font, player):
 		self.window = window
 		self.font = font
 		self.player = player
-		self.monsters = monsters
 
 	def items_draw(self):
 		y = 64  # вывод снаряжения
@@ -15,24 +14,28 @@ class UI:
 				self.window.blit(self.font.render(str(item.stat), True, 'white'), (1280 - 80, y - 24))
 			y += 64
 
-	def write_stats(self):
+	def write_stats(self, monsters):
 		"""
 		Выводит на экран игры все надписи (хп героя, хп монстра, кд, хил, результат)
 		:return: None
 		"""
-		health = self.font.render('Player: ' + (str(self.player.health).split('.')[0] + 'hp' if self.player.health > 0 else 'Dead'),
-		                          False, (0, 0, 0), (0, 150, 50))
-		self.window.blit(health, (0, 0))
 
-		heat = self.font.render('Ready to heat: ' + str(self.player.timer <= 0), False, (0, 0, 0), (200, 100, 100))
-		self.window.blit(heat, (0, 33))
+		# Отрисовка полоски здоровья игрока
+		pygame.draw.rect(self.window, '#222222', (10, 10, 250, 20))
+		pygame.draw.rect(self.window, 'red', (10, 10, self.player.health / 2, 20))
+		pygame.draw.rect(self.window, '#111111', (10, 10, 250, 20), 3)
 
-		score = self.font.render('Score: ' + str(self.player.score), False, (0, 0, 0), (200, 200, 0))
-		self.window.blit(score, (580, 0))
+		# Отрисовка опыта
+		text_surf = self.font.render(str(self.player.score), False, '#EEEEEE')
+		text_rect = text_surf.get_rect(bottomright=(1260, 700))
+		pygame.draw.rect(self.window, '#222222', text_rect.inflate(20, 20))
+		pygame.draw.rect(self.window, '#222222', text_rect.inflate(20, 20), 3)
+		self.window.blit(text_surf, text_rect)
 
-		monster_hp = self.font.render('Monster: ' + (str(self.monsters.hp) + 'hp' if self.monsters.hp > 0 else 'Dead'),
-		                              False, (0, 0, 0), (0, 150, 100))
-		self.window.blit(monster_hp, (0, 685))
+		# Отрисовка полоски здоровья моба
+		pygame.draw.rect(self.window, '#222222', (10, 690, monsters.max_hp, 20))
+		pygame.draw.rect(self.window, (0, 150, 100), (10, 690, monsters.hp, 20))
+		pygame.draw.rect(self.window, '#111111', (10, 690, monsters.max_hp, 20), 3)
 
 	def inventory_draw(self):
 		"""
@@ -57,4 +60,3 @@ class UI:
 			self.window.blit(potion_inventory, (self.window.get_width() // 2 + 32, self.window.get_height() - 64))
 			self.window.blit(self.font.render(str(self.player.items_inventory[2]), True, (200, 200, 200)),
 			            (self.window.get_width() // 2 + 32 + 48, self.window.get_height() - 64 + 32))
-
